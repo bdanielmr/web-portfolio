cdaf034de353f1bffc6c2501543741e19f9e9ea7
bdaniel <bdanielmr@gmail.com>
2021-08-16 11:51:03 -0500

    test styles mobile fix width and heigth

--- src/components/CustomCard/CustomCard.jsx
+++ src/components/CustomCard/CustomCard.jsx
@@ -19,7 +19,7 @@ const CustomCard = ({ imageBackground = "", title = "" }) => {
     setShowModal(false);
   };
   return (
-    <>
+    <div style={{ width: "100%", height: "100vh" }}>
       {showModal && <CustomModal close={handleCloseModal} />}
       <div
         onClick={handleClickModal}
@@ -31,7 +31,7 @@ const CustomCard = ({ imageBackground = "", title = "" }) => {
         <div className={`${styles.blur_back + " " + styles.bright_back}`}></div>
         <button></button>
       </div>
-    </>
+    </div>
   );
 };
 
