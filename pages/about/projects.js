7c1d25feaec86bcfe7e54ee5cde1d83bfdc9ee9c
bdaniel <bdanielmr@gmail.com>
2021-09-02 15:07:52 -0500

    fix top

--- pages/about/projects.js
+++ pages/about/projects.js
@@ -12,7 +12,7 @@ const projects = () => {
         alignItems: "center",
         width: "100%",
         height: "100%",
-        margin: "10% 0%",
+        margin: "6% 0%",
       }}
     >
       <CustomWorkbook />
