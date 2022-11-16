798fb5366d5fd3117d43ce9e69e8812431f81d53
bdanielmr <bdanielmr@gmail.com>
2022-11-15 22:59:23 -0500

    fix: config typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 /* eslint-disable react/prop-types */
+import React from "react";
 import CustomHeader from "../src/components/CustomHeader/CustomHeader";
+import i18nInit from "../src/i18n/i18n";
+
 import "../styles/globals.css";
 
+const translationEs = { home: { title: "test"} }
 function MyApp({ Component, pageProps }) {
+  i18nInit()
   return (
     <>
       <CustomHeader />
