36cf6124ccf4f91222df5c10ada7ed35d9f40af8
bdanielmr <bdanielmr@gmail.com>
2022-11-15 22:58:42 -0500

    feat: add config i18next and add home translates

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import Head from "next/head";
+import Image from "next/image";
+import HomePageView from "../src/Views/HomePageView/HomePageView";
+import React from "react";
+export default function Home() {
+  return (
+    <div>
+      <HomePageView />
+    </div>
+  );
+}
